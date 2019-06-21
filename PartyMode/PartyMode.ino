b4226586c1edaaa8c83e913de3aa82636a13fc23
Michael Lema <mel526@nyu.edu>
2019-06-21 11:54:10 -0400

    Enabled Wifi

--- PartyMode/PartyMode.ino
+++ PartyMode/PartyMode.ino
@@ -5,21 +5,110 @@
  */
 #include "Arduino.h"
 #include "heltec.h"
+#include <WiFi.h>
 #include <Streaming.h>
 #include "ADXL335.h"
+#include "images.h"
 
 int rLED = 27;
-int yLED = 26;
+int yLED = 17;
 int gLED = 25;
-int bLED = 16;
-int wLED = 17;
+int bLED = 2;
+int wLED = 23;
 
 float prevX, prevY, prevZ;
-int lightDelay = 45;
+int lightDelay = 30;
 ADXL335 accelerometer;
+
+/*******************************************************************
+ * OLED Arguments
+ */
+#define OLED_UPDATE_INTERVAL 500        
+
+/**********************************************  WIFI Client *********************************
+ * wifi client
+ */
+const char* ssid = "XXXX"; 
+const char* password = "XXXX"; 
+
+/********************************************************************
+ * setup oled
+ */
+void setupOLED()
+{
+  pinMode(RST_OLED, OUTPUT);
+  //OLED
+  digitalWrite(RST_OLED, LOW);        // turn D16 low to reset OLED
+  delay(50);
+  digitalWrite(RST_OLED, HIGH);       // while OLED is running, must set D16 in high
+  
+  Heltec.display->init();
+  Heltec.display->flipScreenVertically();           //倒过来显示内容
+  Heltec.display->setFont(ArialMT_Plain_10);        //设置字体大小
+  Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);//设置字体对齐方式
+
+  Heltec.display->clear();
+  Heltec.display->drawString(0, 0, "Initialize...");
+}
+
+/*********************************************************************
+ * setup wifi
+ */
+ void drawWifiImage() {
+    // see http://blog.squix.org/2015/05/esp8266-nodemcu-how-to-create-xbm.html
+    // on how to create xbm files
+    Heltec.display->drawXbm(34, 14, WiFi_Logo_width, WiFi_Logo_height, WiFi_Logo_bits);
+}
+
+void setupWIFI()
+{
+  Heltec.display->clear();
+  Heltec.display->drawString(0, 0, "Connecting...");
+  Heltec.display->drawString(0, 10, String(ssid));
+  Heltec.display->display();
+  
+
+  WiFi.disconnect(true);
+  delay(1000);
+  
+  WiFi.mode(WIFI_STA);  
+  WiFi.setAutoConnect(true);      
+  WiFi.setAutoReconnect(true);    
+  WiFi.begin(ssid, password);
+
+  byte count = 0;
+  while(WiFi.status() != WL_CONNECTED && count < 10)
+  {
+    count ++;
+    delay(500);
+    Serial.print(".");
+  }
+
+  Heltec.display->clear();
+  if(WiFi.status() == WL_CONNECTED) 
+  {
+    Heltec.display->drawString(0, 0, "Connecting...OK."); 
+    Serial.println("Done!");
+    // clear the display
+    Heltec.display->clear();
+    drawWifiImage();
+    Heltec.display->setTextAlignment(TEXT_ALIGN_RIGHT);
+    Heltec.display->drawString(10, 128, String(millis()));
+    // write the buffer to the display
+    Heltec.display->display();
+  }  
+  else {
+    Heltec.display->drawString(0, 0, "Connecting...Failed");
+  }
+  Heltec.display->display();
+}
+
 
 void setup()
 {
+  Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Disable*/, false /*Serial Enable*/);
+  Serial.println("Initialize...");
+
   Serial.begin(115200);
   accelerometer.begin();
   pinMode(rLED, OUTPUT);
@@ -27,10 +116,11 @@ void setup()
   pinMode(gLED, OUTPUT);
   pinMode(bLED, OUTPUT);
   pinMode(wLED, OUTPUT);
+  setupOLED();
+  setupWIFI();
 }
 
 void lightShow() {
-    Serial.println("SHOW ME!!");
     digitalWrite(rLED, HIGH);
     delay(lightDelay);
     digitalWrite(yLED, HIGH);
@@ -60,7 +150,7 @@ void loop()
 {
   float ax,ay,az;
   accelerometer.getAcceleration(&ax,&ay,&az);
- 
+ /*
   Serial.println("accleration of X/Y/Z: ");
   Serial.print(ax);
   Serial.println(" g");
@@ -68,12 +158,15 @@ void loop()
   Serial.println(" g");
   Serial.print(az);
   Serial.println(" g");
-  if ( abs(ax-prevX) > 1 || abs(ay-prevY) > 1 || abs(az-prevZ) > 1 ) {
+  */
+  
+  if ( abs(ay-prevY) >= 4.0 || abs(az-prevZ) >= 4.0 ) {
+    //Serial <<  "x: " << ax  << " "<< prevX <<  " " << abs(ax-prevX) << " y:" << abs(ay-prevY) << " z: " << abs(az-prevZ) << endl;
     lightShow();
+    delay(300);
   }
    prevX = ax;
    prevY = ay;
    prevZ = az;
    
-   delay(300);
 }
